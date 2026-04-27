1e2b10b77c36c262b0d73efaa2f387933a1a8a82
lohan <lohanvictorlimaaa@gmail.com>
2026-04-27 13:05:04 -0300

    gestao escolar

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # instalar as bibliotecas:
 pip install fastapi uvicorn sqlalchemy alembic python-dotenv
+pip install jinja2 python-multipart
 
 # Rodar o fastapi:
 # No terminal : python -m uvicorn main:app --reload 
--- main.py
+++ main.py
@@ -24,3 +24,53 @@ templates = Jinja2Templates(directory="templates")
 @app.get("/cursos/cadastro", response_class=HTMLResponse)
 def exibir_cadastro(request: Request):
     return templates.TemplateResponse(request, "cadastro_curso.html", {"request": request})
+
+#Rota inicial para apresentação
+@app.get("/")
+def home(request: Request):
+    return templates.TemplateResponse(
+        request,
+        "index.html",
+        {"request": request}
+    )
+#Cadastrar curso
+@app.post("/cursos")
+def criar_curso(
+    nome: str = Form(...),
+    carga_horaria: int = Form(...),
+    descricao: str = Form(...),
+    db: Session = Depends(get_db)
+):
+    #Criar um objeto
+    novo_curso = Curso(nome=nome, carga_horaria=carga_horaria, descricao=descricao)
+    db.add(novo_curso)
+    db.commit()
+
+    return RedirectResponse(url="/listar_cursos", status_code=303)
+
+#listar cursos 
+@app.get("/listar_cursos")
+def exibir_cursos(
+    request: Request,
+    db: Session = Depends(get_db)
+    ):
+    cursos = db.query(Curso).all()
+    return templates.TemplatesResponse(
+        request,
+        "cursos.html", 
+        {"request": request, "cursos": cursos}
+    )
+
+
+#Rota para deletar um curso
+@app.post("/cursos/{id}/deletar")
+def deletar_curso(
+    id: int, 
+    db: Session = Depends(get_db)
+):
+    curso = db.query(Curso).filter_by(id=id)
+    if curso:
+        db.delete(curso)
+        db.commit()
+    
+    return RedirectResponse(url="/listar_cursos", status_code=303)
